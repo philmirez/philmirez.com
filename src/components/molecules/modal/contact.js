f5480e8e9656452c44633a58354394573aa077c1
Phil Ramirez <philmirez@gmail.com>
2019-06-22 17:19:58 -0400

    Contact form refactor

--- src/components/molecules/modal/contact.js
+++ src/components/molecules/modal/contact.js
@@ -4,6 +4,49 @@ import MessageTwoTone from '@material-ui/icons/MessageTwoTone'
 import Grid from '@material-ui/core/Grid'
 import { Card } from 'layout/style'
 import Modal from 'react-rainbow-components/components/Modal'
+import Contact from 'molecules/forms/contact'
+
+const oldContact = () => (
+  <React.Fragment>
+    <h3>ᕕ(ᐛ)ᕗ Either send me an email or message me on LinkedIn</h3>
+    <a href='mailto:philmirez@gmail.com?Subject=Hey%20Phil' target='_top'>
+      <Card>
+        <Grid container>
+          <Grid item xs={12}>
+            <Grid
+              container
+              direction='row'
+              justify='flex-start'
+              alignItems='flex-start'
+            >
+              <EmailTwoTone className='modalEmail' /> Gmail
+            </Grid>
+          </Grid>
+        </Grid>
+      </Card>
+    </a>
+    <a
+      href='https://www.linkedin.com/in/philmirez'
+      target='_blank'
+      rel='noopener noreferrer'
+    >
+      <Card>
+        <Grid container>
+          <Grid item xs={12}>
+            <Grid
+              container
+              direction='row'
+              justify='flex-start'
+              alignItems='flex-start'
+            >
+              <MessageTwoTone className='modalBrandLinkedin' /> LinkedIn
+            </Grid>
+          </Grid>
+        </Grid>
+      </Card>
+    </a>
+  </React.Fragment>
+)
 
 export default class extends React.Component {
   render () {
@@ -15,43 +58,7 @@ export default class extends React.Component {
         title='Contact'
         onRequestClose={onCloseClick}
       >
-        <h3>ᕕ(ᐛ)ᕗ Either send me an email or message me on LinkedIn</h3>
-        <a href='mailto:philmirez@gmail.com?Subject=Hey%20Phil' target='_top'>
-          <Card>
-            <Grid container>
-              <Grid item xs={12}>
-                <Grid
-                  container
-                  direction='row'
-                  justify='flex-start'
-                  alignItems='flex-start'
-                >
-                  <EmailTwoTone className='modalEmail' /> Gmail
-                </Grid>
-              </Grid>
-            </Grid>
-          </Card>
-        </a>
-        <a
-          href='https://www.linkedin.com/in/philmirez'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <Card>
-            <Grid container>
-              <Grid item xs={12}>
-                <Grid
-                  container
-                  direction='row'
-                  justify='flex-start'
-                  alignItems='flex-start'
-                >
-                  <MessageTwoTone className='modalBrandLinkedin' /> LinkedIn
-                </Grid>
-              </Grid>
-            </Grid>
-          </Card>
-        </a>
+        <Contact />
       </Modal>
     )
   }
